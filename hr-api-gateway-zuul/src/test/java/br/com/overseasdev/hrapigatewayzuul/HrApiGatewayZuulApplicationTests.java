1347b15b9b911d8572f2dbacd3da2055c7e0c979
Matheus Cunha <matheus.overseas@gmail.com>
2023-02-09 18:53:48 -0300

    Configuring Zuul API Gateway

--- hr-api-gateway-zuul/src/test/java/br/com/overseasdev/hrapigatewayzuul/HrApiGatewayZuulApplicationTests.java
+++ hr-api-gateway-zuul/src/test/java/br/com/overseasdev/hrapigatewayzuul/HrApiGatewayZuulApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.hrapigatewayzuul;
+package br.com.overseasdev.hrapigatewayzuul;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
