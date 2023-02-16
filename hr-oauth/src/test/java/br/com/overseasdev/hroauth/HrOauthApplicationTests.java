626cc84832ae8a5acc0120ff4da8b75ec8b52098
Matheus Cunha <matheus.overseas@gmail.com>
2023-02-16 17:56:53 -0300

    Project hr-oauth created

---
+++ hr-oauth/src/test/java/br/com/overseasdev/hroauth/HrOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.overseasdev.hroauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrOauthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
