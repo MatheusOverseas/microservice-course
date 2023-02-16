f7b0400b7b6fe3790facf008740af07d5326c586
Matheus Cunha <matheus.overseas@gmail.com>
2023-02-16 17:21:10 -0300

    Database seeding

--- hr-user/src/main/java/br/com/overseasdev/hruser/entities/Role.java
+++ hr-user/src/main/java/br/com/overseasdev/hruser/entities/Role.java
@@ -9,7 +9,7 @@ import java.util.Objects;
 public class Role implements Serializable {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String roleName;
 
